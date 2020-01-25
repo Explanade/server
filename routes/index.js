373be2e5f193de7d1ff28b9480cf032b76066fd5
Angela Vanessa <hansavanessa@gmail.com>
2020-01-25 13:43:57 +0800

    google places api server

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,13 @@ const user = require('./user')
 const itinerary = require('./itinerary')
 const activity = require('./activity')
 const review = require('./review')
+const google = require('./google')
 
 router.get('/', (req, res) => res.send(`Hello World - from Explanade <3`))
 router.use('/user', user)
 router.use('/itineraries', itinerary)
 router.use('/activities', activity)
 router.use('/reviews', review)
-
+router.use('/google', google)
 
 module.exports = router
