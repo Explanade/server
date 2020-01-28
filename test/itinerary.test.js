f408e0a23509c91060db483ccbd448dd099ff971
hoandreas <andreas_ho@hotmail.com>
2020-01-28 16:52:25 +0700

    done

--- test/itinerary.test.js
+++ test/itinerary.test.js
@@ -3,8 +3,6 @@ const chaiHttp = require('chai-http')
 const app = require('../app')
 const User = require('../models/user')
 const Itinerary = require('../models/itinerary')
-const Activity = require('../models/activity')
-const ItineraryController = require('../controllers/itinerary')
 const { generateToken } = require('../helpers/jwt')
 
 chai.use(chaiHttp)
@@ -32,8 +30,8 @@ let itineraryData = {
   name: 'My Awesome Trip',
   location: {
     name: "Bandung",
-    lat: -6.903429,
-    lng: 107.5030708
+		lat: -6.903429,
+		lng: 107.5030708
   },
   start_date: '01/20/2020',
   end_date: '01/22/2020'
@@ -72,14 +70,8 @@ before(function(done) {
         name: user.name,
         email: user.email
       })
-      const completeItineraryData = { ...itineraryData }
-      completeItineraryData.user_id = user._id
-      return Itinerary.create(completeItineraryData)
-    })
-    .then(itinerary => {
-      let dummyActivityData = { ...activityData }
-      dummyActivityData.itinerary_id = itinerary._id
       return User.create(secondUser)
+    })
     .then(user2 => {
       userToken2 = generateToken({
         id: user2._id,
@@ -88,12 +80,11 @@ before(function(done) {
       })
       done()
     })
-    })
     .catch(console.log)
 })
 
-after(function (done) {
-  if (process.env.NODE_ENV === 'test') {
+after(function(done) {
+  if(process.env.NODE_ENV === 'test') {
     User.deleteMany({})
       .then(_ => {
         console.log('testing: delete data user success!')
@@ -107,7 +98,7 @@ after(function (done) {
   }
 })
 
-describe('CRD Itinerary Endpoints', function () {
+describe('CRD Itinerary Endpoints', function() {
   describe('POST /itineraries', () => {
     describe('success process', () => {
       it('should return an object (data) with status code 201', (done) => {
@@ -129,75 +120,75 @@ describe('CRD Itinerary Endpoints', function () {
     describe('error process', () => {
       it('should send an error with 401 status code because missing token', (done) => {
         chai.request(app)
-          .post('/itineraries')
-          .set('Content-Type', 'application/json')
-          .send(itineraryData)
-          .end(function (err, res) {
-            expect(err).to.be.null
-            expect(res).to.have.status(401)
-            expect(res.body.message[0]).to.equal("you have to login first")
-            done()
-          })
+        .post('/itineraries')
+        .set('Content-Type', 'application/json')
+        .send(itineraryData)
+        .end(function(err, res) {
+          expect(err).to.be.null
+          expect(res).to.have.status(401)
+          expect(res.body.message[0]).to.equal("you have to login first")
+          done()
+        })
       })
       it('should send an error with 400 status code because missing itinerary\'s name', (done) => {
         const withoutName = { ...itineraryData }
         delete withoutName.name
         chai.request(app)
-          .post('/itineraries')
-          .set('token', userToken)
-          .set('Content-Type', 'application/json')
-          .send(withoutName)
-          .end(function (err, res) {
-            expect(err).to.be.null
-            expect(res).to.have.status(400)
-            expect(res.body.message).to.equal("bad request")
-            done()
-          })
+        .post('/itineraries')
+        .set('token', userToken)
+        .set('Content-Type', 'application/json')
+        .send(withoutName)
+        .end(function(err, res) {
+          expect(err).to.be.null
+          expect(res).to.have.status(400)
+          expect(res.body.message).to.equal("bad request")
+          done()
+        })
       })
       it('should send an error with 400 status code because missing itinerary\'s location', (done) => {
         const withoutLocation = { ...itineraryData }
         delete withoutLocation.location
         chai.request(app)
-          .post('/itineraries')
-          .set('token', userToken)
-          .set('Content-Type', 'application/json')
-          .send(withoutLocation)
-          .end(function (err, res) {
-            expect(err).to.be.null
-            expect(res).to.have.status(400)
-            expect(res.body.message).to.equal("bad request")
-            done()
-          })
+        .post('/itineraries')
+        .set('token', userToken)
+        .set('Content-Type', 'application/json')
+        .send(withoutLocation)
+        .end(function(err, res) {
+          expect(err).to.be.null
+          expect(res).to.have.status(400)
+          expect(res.body.message).to.equal("bad request")
+          done()
+        })
       })
       it('should send an error with 400 status code because missing itinerary\'s start date', (done) => {
         const withoutStartDate = { ...itineraryData }
         delete withoutStartDate.start_date
         chai.request(app)
-          .post('/itineraries')
-          .set('token', userToken)
-          .set('Content-Type', 'application/json')
-          .send(withoutStartDate)
-          .end(function (err, res) {
-            expect(err).to.be.null
-            expect(res).to.have.status(400)
-            expect(res.body.message).to.equal("bad request")
-            done()
-          })
+        .post('/itineraries')
+        .set('token', userToken)
+        .set('Content-Type', 'application/json')
+        .send(withoutStartDate)
+        .end(function(err, res) {
+          expect(err).to.be.null
+          expect(res).to.have.status(400)
+          expect(res.body.message).to.equal("bad request")
+          done()
+        })
       })
       it('should send an error with 400 status code because missing itinerary\'s end date', (done) => {
         const withoutEndDate = { ...itineraryData }
         delete withoutEndDate.end_date
         chai.request(app)
-          .post('/itineraries')
-          .set('token', userToken)
-          .set('Content-Type', 'application/json')
-          .send(withoutEndDate)
-          .end(function (err, res) {
-            expect(err).to.be.null
-            expect(res).to.have.status(400)
-            expect(res.body.message).to.equal("bad request")
-            done()
-          })
+        .post('/itineraries')
+        .set('token', userToken)
+        .set('Content-Type', 'application/json')
+        .send(withoutEndDate)
+        .end(function(err, res) {
+          expect(err).to.be.null
+          expect(res).to.have.status(400)
+          expect(res.body.message).to.equal("bad request")
+          done()
+        })
       })
     })
   })
@@ -239,25 +230,25 @@ describe('CRD Itinerary Endpoints', function () {
     describe('success process', () => {
       it('should send an array of object with status code 200', (done) => {
         chai.request(app)
-          .get('/itineraries')
-          .end(function (err, res) {
-            expect(err).to.be.null
-            expect(res).to.have.status(200)
-            expect(res.body).to.be.an('array')
-            done()
-          })
+        .get('/itineraries')
+        .end(function(err, res) {
+          expect(err).to.be.null
+          expect(res).to.have.status(200)
+          expect(res.body).to.be.an('array')
+          done()
+        })
       })
     })
     describe('error process', () => {
       it('should send a status code 404 cause of wrong endpoint', (done) => {
         chai.request(app)
-          .get('/itinerary')
-          .end(function (err, res) {
-            expect(err).to.be.null
-            expect(res).to.have.status(404)
-            expect(res.body).to.be.an('object')
-            done()
-          })
+        .get('/itinerary')
+        .end(function(err, res) {
+          expect(err).to.be.null
+          expect(res).to.have.status(404)
+          expect(res.body).to.be.an('object')
+          done()
+        })
       })
     })
   })
@@ -265,26 +256,26 @@ describe('CRD Itinerary Endpoints', function () {
     describe('success process', () => {
       it('should send an object with status code 200', (done) => {
         chai.request(app)
-          .get('/itineraries/' + dummyItinerary._id)
-          .end(function (err, res) {
-            expect(err).to.be.null
-            expect(res).to.have.status(200)
-            expect(res.body).to.be.an('object')
-            done()
-          })
+        .get('/itineraries/' + dummyItinerary._id)
+        .end(function(err, res) {
+          expect(err).to.be.null
+          expect(res).to.have.status(200)
+          expect(res.body).to.be.an('object')
+          done()
+        })
       })
     })
     describe('error process', () => {
       it('should send an object with message itinerary not found and status code 404 because of invalid id', (done) => {
         chai.request(app)
-          .get('/itineraries/' + invalidItineraryId)
-          .end(function (err, res) {
-            expect(err).to.be.null
-            expect(res).to.have.status(404)
-            expect(res.body).to.be.an('object').to.have.any.keys('message')
-            expect(res.body.message).to.equal('itinerary not found')
-            done()
-          })
+        .get('/itineraries/' + invalidItineraryId)
+        .end(function(err, res) {
+          expect(err).to.be.null
+          expect(res).to.have.status(404)
+          expect(res.body).to.be.an('object').to.have.any.keys('message')
+          expect(res.body.message).to.equal('itinerary not found')
+          done()
+        })
       })
     })
   })
@@ -292,141 +283,75 @@ describe('CRD Itinerary Endpoints', function () {
     describe('success process', () => {
       it('should send an array of object with status code 200', (done) => {
         chai.request(app)
-          .get('/itineraries/my-itineraries')
-          .set('token', userToken)
-          .end(function (err, res) {
-            expect(err).to.be.null
-            expect(res).to.have.status(200)
-            expect(res.body).to.be.an('array')
-            done()
-          })
+        .get('/itineraries/my-itineraries')
+        .set('token', userToken)
+        .end(function(err, res) {
+          expect(err).to.be.null
+          expect(res).to.have.status(200)
+          expect(res.body).to.be.an('array')
+          done()
+        })
       })
     })
     describe('error process', () => {
       it('should send a status code 401 cause of missing token', (done) => {
         chai.request(app)
-          .get('/itineraries/my-itineraries')
-          .end(function (err, res) {
-            expect(err).to.be.null
-            expect(res).to.have.status(401)
-            expect(res.body.message[0]).to.equal("you have to login first")
-            done()
-          })
+        .get('/itineraries/my-itineraries')
+        .end(function(err, res) {
+          expect(err).to.be.null
+          expect(res).to.have.status(401)
+          expect(res.body.message[0]).to.equal("you have to login first")
+          done()
+        })
       })
       it('should send a status code 404 cause of wrong endpoint', (done) => {
         chai.request(app)
-          .get('/itineraries/my-itinerary')
-          .end(function (err, res) {
-            expect(err).to.be.null
-            expect(res).to.have.status(404)
-            expect(res.body).to.be.an('object')
-            done()
-          })
+        .get('/itineraries/my-itinerary')
+        .end(function(err, res) {
+          expect(err).to.be.null
+          expect(res).to.have.status(404)
+          expect(res.body).to.be.an('object')
+          done()
+        })
       })
     })
   })
-
-  // =============================================== ITINERARY UPDATE
-
-  // describe('UPDATE /itineraries/:id', () => {
-  //   console.log(dummyItinerary)
-
-  //   describe('error process', () => {
-  //     it('should send an object with message You are not authorized to perform this action and status code 403 because of invalid token', (done) => {
-  //       chai.request(app)
-  //         .put('/itineraries/' + dummyItinerary._id)
-  //         .set('token', userToken2)
-  //         .send(dummyItinerary)
-  //         .end((err, res) => {
-  //           expect(err).to.be.null
-  //           expect(res).to.have.status(403)
-  //           done()
-  //         })
-  //     })
-  //     it('should send status code 404 and error message Itinerary not found cause of invalid itinerary id', (done) => {
-  //       chai.request(app)
-  //         .put('/itineraries/' + invalidItineraryId)
-  //         .set('token', userToken)
-  //         .end((err, res) => {
-  //           expect(err).to.be.null
-  //           expect(res).to.have.status(404)
-  //           expect(res.body).to.be.an('object').to.have.any.keys('message')
-  //           expect(res.body.message).to.equal('Itinerary not found')
-  //           done()
-  //         })
-  //     })
-  //   })
-  //   describe('success process', () => {
-  //     it('should send status code 204', (done) => {
-  //       console.log(completeItin)
-  //       chai.request(app)
-  //         .put('/itineraries/' + completeItin._id)
-  //         .set('token', userToken)
-  //         .send(completeItin)
-  //         .end((err, res) => {
-  //           expect(err).to.be.null
-  //           expect(res).to.have.status(200)
-  //           done()
-  //         })
-  //     })
-  //   })
-  //   describe('success process', () => {
-  //     let data = { ...completeItin }
-  //     it('should send status code 204', (done) => {
-  //       chai.request(app)
-  //         .put('/itineraries/' + completeItin._id)
-  //         .set('token', userToken)
-  //         .send(completeItin)
-  //         .end((err, res) => {
-  //           expect(err).to.be.null
-  //           expect(res).to.have.status(200)
-  //           done()
-  //         })
-  //     })
-  //   })
-  // })
-
-
-  // ================================================
-
-
   describe('DELETE /itineraries/:id', () => {
     describe('error process', () => {
       it('should send an object with message You are not authorized to perform this action and status code 403 because of invalid token', (done) => {
         chai.request(app)
-          .delete('/itineraries/' + dummyItinerary._id)
-          .set('token', userToken2)
-          .end((err, res) => {
-            expect(err).to.be.null
-            expect(res).to.have.status(403)
-            done()
-          })
+        .delete('/itineraries/' + dummyItinerary._id)
+        .set('token', userToken2)
+        .end((err, res) => {
+          expect(err).to.be.null
+          expect(res).to.have.status(403)
+          done()
+        })
       })
       it('should send status code 404 and error message Itinerary not found cause of invalid itinerary id', (done) => {
         chai.request(app)
-          .delete('/itineraries/' + invalidItineraryId)
-          .set('token', userToken)
-          .end((err, res) => {
-            expect(err).to.be.null
-            expect(res).to.have.status(404)
-            expect(res.body).to.be.an('object').to.have.any.keys('message')
-            expect(res.body.message).to.equal('Itinerary not found')
-            done()
-          })
+        .delete('/itineraries/' + invalidItineraryId)
+        .set('token', userToken)
+        .end((err, res) => {
+          expect(err).to.be.null
+          expect(res).to.have.status(404)
+          expect(res.body).to.be.an('object').to.have.any.keys('message')
+          expect(res.body.message).to.equal('Itinerary not found')
+          done()
+        })
       })
     })
     describe('success process', () => {
       it('should send status code 204', (done) => {
         chai.request(app)
-          .delete('/itineraries/' + dummyItinerary._id)
-          .set('token', userToken)
-          .end((err, res) => {
-            expect(err).to.be.null
-            expect(res).to.have.status(204)
-            done()
-          })
+        .delete('/itineraries/' + dummyItinerary._id)
+        .set('token', userToken)
+        .end((err, res) => {
+          expect(err).to.be.null
+          expect(res).to.have.status(204)
+          done()
+        })
       })
     })
   })
-
 })
